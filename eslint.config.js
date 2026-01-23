339312dcde9248f04dadb4b9fc5aabc922ca32a8
Kenji Sakuma <kenjchan2011@gmail.com>
2026-01-23 11:18:27 -0800

    Added '--max-warning=0' in the Check linting section

--- eslint.config.js
+++ eslint.config.js
@@ -2,13 +2,19 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import { defineConfig } from "eslint/config";
-  
-  export default defineConfig(
-    [
-      { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-        tseslint.configs.recommended,
-      {
-        ignores: ["dist/**", "coverage/**", "node_modules/**"]
-      }
-    ]
-  );
+import pluginSecurity from "eslint-plugin-security";
+
+
+  export default defineConfig([
+    { 
+      files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], 
+      plugins: { js }, 
+      extends: ["js/recommended"], 
+      languageOptions: { globals: globals.browser } 
+    },
+    tseslint.configs.recommended,
+    pluginSecurity.configs.recommended,
+    {
+      ignores: ["dist/**", "coverage/**", "node_modules/**"]
+    }
+]);
